8880b6e22854730e94c32e4dc8cf8b7c2b17e4ec
cdcd72 <cdcd71517@gmail.com>
2023-11-16 17:44:05 +0800

    Modify simple demo

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import './App.scss';
 
 import { Calendar } from '@progress/kendo-react-dateinputs';
 
+import { SvgIcon } from '@progress/kendo-react-common';
+import { calendarIcon } from '@progress/kendo-svg-icons';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello Kendo React!</h1>
+      <h1>
+        Hello Kendo Calendar <SvgIcon icon={calendarIcon} size="xxlarge" />
+      </h1>
       <Calendar />
     </div>
   );
